62ffddd10c36b56f83253e7049c9fd2f072e1b87
Shivangi Dutta <shivangidutta4175@gmail.com>
2025-05-19 23:54:22 +0530

    Initial Commit

---
+++ BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by Shivangi Dutta on 19/05/25.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
